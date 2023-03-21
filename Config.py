3dcbb6e4fd54c5cf1e3c47f4986b0e38547b8bf2
jiomusicclub <128532313+jiomusicclub@users.noreply.github.com>
2023-03-22 02:07:55 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -3,8 +3,8 @@ import os
 class Config(object):
     API_ID = int(os.environ.get("API_ID", "6213538"))
     API_HASH = os.environ.get("API_HASH", "8ce3522bd21cc937eee4c68813d501d5")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-    STRING_SESSION = os.environ.get("STRING_SESSION", None)
+    BOT_TOKEN = ","6289175472:AAHvwwTpchbbHxYk10Q_zF-eCCtWqjKA0_U None)
+    STRING_SESSION = 1BVtsOJQBu61mqs_3wKaA9tywoo3B88el3ytRiHk4BRlfAxyCdYAI8kZSkPxeFbBaFmmY4z3GOYEXyFLP-zWof_HLfKJC9v2t5daWNoNhpBtO6O_jAaSbfHyJj34xgFn0EZe3_q8_hag97qPVCsKxHqlLEG7EDjvCcoOyYWAqFgz9IkiJrXJrQSSNLyGk17ll76pTcF8iz9wPrTKHQlNVZQoBfGCZOyOYnozaqsfCdcT_LWgfpyD8BXa7IFf5uSnoceecOCvfhNRxpmXm8uCmI5XQKjcUxS2sJhKYkh7FIDijQ0yd4B9-ZN6KrUj0rd3s6nBOnIVl9Nl7BZXMBEFt7JjRPGtBizk= None)
     MANAGEMENT_MODE = os.environ.get("MANAGEMENT_MODE", None)
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
